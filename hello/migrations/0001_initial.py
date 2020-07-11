3203c2b19a47cda874ae86580fbaf8c670764fbd
gnoeyhad <da0108hy@naver.com>
2020-07-11 18:06:03 +0900

    Second commit

---
+++ hello/migrations/0001_initial.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Blog',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
+                ('title', models.CharField(max_length=200)),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('updated_at', models.DateTimeField(auto_now=True)),
+                ('body', models.TextField()),
+            ],
+        ),
+    ]
--- hello/views.py
+++ hello/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, redirect
+from .models import Blog
 
 def home(request):
-    return render(request, 'home.html')
+    blogs = Blog.objects
+    return render(request, 'home.html', {'blogs' : blogs})
+
+def new(request):
+    return render(request, 'new.html')
+
+def create(request):
+    if(request.method == 'POST'):
+        post = Blog()
+        post.title = request.POST['title']
+        post.body = request.POST['body']
+        post.save()
+    return redirect('home')
